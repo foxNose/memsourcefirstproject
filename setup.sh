7cb117ce685851e8f3ddf78cfdf0b669129053d6
Dominik <tousekdominik@gmail.com>
2021-03-17 08:28:46 +0100

    Fixes after tests on other environment * Updated setup.sh * SessionMock to test/unit - grails was having problems locating class on other systems

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ source .env
 # Install Java JDK
 sudo apt install openjdk-${JAVA_VERSION}-jdk
 # Install grails
-curl -s https://get.sdkman.io
-source "${HOME}/.sdkman/bin/sdkman-init.sh"
+curl -s https://get.sdkman.io | bash
+source "${SDKMAN_DIR}/.sdkman/bin/sdkman-init.sh"
 sdk install grails ${GRAILS_VERSION}
